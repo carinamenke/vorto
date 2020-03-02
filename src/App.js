36f232474b061219f13d60467c6c3288ec11a866
carinamenke <carina-menke@live.de>
2020-03-02 15:32:09 +0100

    Build Card.js component and insert it in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,79 @@
 import React from 'react'
+import Card from './Card/Card'
+import DoorImage from './images/door.jpg'
+import WindowImage from './images/window.jpg'
+import ChairImage from './images/chair.jpg'
+import DefaultImage from './images/default.png'
+import DrinkImage from './images/drink.jpg'
+import CosyImage from './images/cosy.jpg'
+import PlateImage from './images/plate.jpg'
+import GlassImage from './images/glass.jpg'
+import SpaciousImage from './images/spacious.jpg'
+import ForkImage from './images/fork.jpg'
 
-function App() {
-  return <div className="App"></div>
+export default function App() {
+  return (
+    <>
+      <Card
+        vocabImageSrc={DoorImage}
+        vocabImageAltText="A door"
+        vocabTitle="The door"
+        partOfSpeechCategory="Noun"
+      />
+      <Card
+        vocabImageSrc={WindowImage}
+        vocabImageAltText="A window"
+        vocabTitle="The window"
+        partOfSpeechCategory="Noun"
+      />
+      <Card
+        vocabImageSrc={ChairImage}
+        vocabImageAltText="A chair"
+        vocabTitle="The chair"
+        partOfSpeechCategory="Noun"
+      />
+      <Card
+        vocabImageSrc={DefaultImage}
+        vocabImageAltText=""
+        vocabTitle="to sit"
+        partOfSpeechCategory="Verb"
+      />
+      <Card
+        vocabImageSrc={DrinkImage}
+        vocabImageAltText="to drink"
+        vocabTitle="to drink"
+        artOfSpeechCategory="Verb"
+      />
+      <Card
+        vocabImageSrc={CosyImage}
+        vocabImageAltText="cosy"
+        vocabTitle="cosy"
+        partOfSpeechCategory="Adjective"
+      />
+      <Card
+        vocabImageSrc={PlateImage}
+        vocabImageAltText="A plate"
+        vocabTitle="The plate"
+        partOfSpeechCategory="Noun"
+      />
+      <Card
+        vocabImageSrc={GlassImage}
+        vocabImageAltText="A glass"
+        vocabTitle="The glass"
+        partOfSpeechCategory="Noun"
+      />
+      <Card
+        vocabImageSrc={SpaciousImage}
+        vocabImageAltText="A spacious room"
+        vocabTitle="spacious"
+        partOfSpeechCategory="Adjective"
+      />
+      <Card
+        vocabImageSrc={ForkImage}
+        vocabImageAltText="A fork"
+        vocabTitle="The fork"
+        partOfSpeechCategory="Noun"
+      />
+    </>
+  )
 }
-
-export default App
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,3 +1,24 @@
 import React from 'react'
+import styled from 'styled-components/macro'
 
-export default function Card() {}
+export default function Card({
+  vocabImageSrc,
+  vocabImageAltText,
+  vocabTitle,
+  partOfSpeechCategory,
+}) {
+  return (
+    <CardStyled>
+      <img src={vocabImageSrc} alt={vocabImageAltText} />
+      <h1>{vocabTitle}</h1>
+      <span>{partOfSpeechCategory}</span>
+    </CardStyled>
+  )
+}
+
+const CardStyled = styled.div`
+  background: #fff;
+  width: 290px;
+  height: 380px;
+  border-radius: 10%;
+`

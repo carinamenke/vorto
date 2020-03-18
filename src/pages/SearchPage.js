c7b6b6400cb0361d2d9fca7d94c30523462faec6
carinamenke <carina-menke@live.de>
2020-03-18 11:16:30 +0100

    Finalize search functionality with useEffect hook

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from '../components/Search/Search'
 import VocabList from '../components/VocabList/VocabList'
 
 export default function SearchPage({ vocabs }) {
   const [searchInput, setSearchInput] = useState('')
   const [searchResult, setSearchResult] = useState([])
+
+  useEffect(() => {
+    let allVocabs = vocabs.map(vocab => {
+      return {
+        imageSrc: vocab.imageSrc,
+        audioSrc: vocab.audioSrc,
+        wordTitle: vocab.wordTitle.toLowerCase(),
+        translation: vocab.translation.toLowerCase(),
+        partOfSpeechCategory: vocab.partOfSpeechCategory,
+      }
+    })
+    if (searchInput !== '') {
+      let filteredVocabs = []
+      filteredVocabs = allVocabs.filter(
+        vocab =>
+          vocab.wordTitle.includes(searchInput.toLowerCase()) ||
+          vocab.translation.includes(searchInput.toLowerCase())
+      )
+      setSearchResult(filteredVocabs)
+    } else {
+      setSearchResult([])
+    }
+  }, [searchInput, vocabs])
 
   return (
     <>
@@ -19,32 +42,10 @@ export default function SearchPage({ vocabs }) {
   )
 
   function handleSearch(event) {
-    let allVocabs = vocabs.map(vocab => {
-      return {
-        imageSrc: vocab.imageSrc,
-        audioSrc: vocab.audioSrc,
-        wordTitle: vocab.wordTitle.toLowerCase(),
-        translation: vocab.translation.toLowerCase(),
-        partOfSpeechCategory: vocab.partOfSpeechCategory,
-      }
-    })
     setSearchInput(event.target.value)
-    if (searchInput !== '') {
-      let filteredVocabs = []
-      filteredVocabs = allVocabs.filter(
-        vocab =>
-          vocab.wordTitle.includes(searchInput.toLowerCase()) ||
-          vocab.translation.includes(searchInput.toLowerCase())
-      )
-      setSearchResult(filteredVocabs)
-    } else {
-      setSearchResult([])
-    }
   }
 
   function handleReset() {
     return setSearchInput('')
-    return setSearchResult([])
-    // focus?
   }
 }
